9e28647c977f6f9e6a4838c85cb3a7b071fa4154
DESKTOP-99UP9B2\YanJ <kj929409@gmail.com>
2018-08-25 20:20:19 +0800

    done the job

---
+++ ExesAndOhs/ExesAndOhs/Kata.cs
@@ -0,0 +1,34 @@
+﻿namespace ExesAndOhs
+{
+    public class Kata
+    {
+        public static bool XO(string input)
+        {
+            input = input.ToLower();
+            var x = 0;
+            var y = 0;
+            foreach (var target in input)
+            {
+                switch (target)
+                {
+                    case 'o':
+                        x++;
+                        break;
+
+                    case 'x':
+                        y++;
+                        break;
+
+                    default:
+                        continue;
+                }
+            }
+
+            if (x == 0 && y == 0)
+            {
+                return true;
+            }
+            return x == y ? true : false;
+        }
+    }
+}
---
+++ ExesAndOhs/ExesAndOhsTests/KataTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExesAndOhs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExesAndOhs.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void XoTest()
+        {
+            Assert.AreEqual(true, Kata.XO("xo"));
+        }
+        [TestMethod()]
+        public void XoTest2()
+        {
+            Assert.AreEqual(true, Kata.XO("xxOo"));
+        }
+        [TestMethod()]
+        public void XoTest3()
+        {
+            Assert.AreEqual(false, Kata.XO("xxxm"));
+        }
+        [TestMethod()]
+        public void XoTest4()
+        {
+            Assert.AreEqual(false, Kata.XO("Oo"));
+        }
+        [TestMethod()]
+        public void XoTest5()
+        {
+            Assert.AreEqual(false, Kata.XO("ooom"));
+        }
+    }
+}
