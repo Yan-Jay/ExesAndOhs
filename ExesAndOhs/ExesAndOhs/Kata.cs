6d50c4a70cd21d8ba0252e0c89eddceeae7d52f1
DESKTOP-99UP9B2\YanJ <kj929409@gmail.com>
2018-08-25 20:27:14 +0800

    refactor

--- ExesAndOhs/ExesAndOhs/Kata.cs
+++ ExesAndOhs/ExesAndOhs/Kata.cs
@@ -1,34 +1,13 @@
-﻿namespace ExesAndOhs
+﻿using System.Linq;
+
+namespace ExesAndOhs
 {
     public class Kata
     {
         public static bool XO(string input)
         {
             input = input.ToLower();
-            var x = 0;
-            var y = 0;
-            foreach (var target in input)
-            {
-                switch (target)
-                {
-                    case 'o':
-                        x++;
-                        break;
-
-                    case 'x':
-                        y++;
-                        break;
-
-                    default:
-                        continue;
-                }
-            }
-
-            if (x == 0 && y == 0)
-            {
-                return true;
-            }
-            return x == y ? true : false;
+            return input.Count(x => x == 'x') == input.Count(x => x == 'o') ? true : false;
         }
     }
 }
